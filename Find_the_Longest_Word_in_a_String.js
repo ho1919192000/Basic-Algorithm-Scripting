4ec877343d23dce4d272d5cb146934f6a727fe3b
ho1919192000 <ho1919192000@gmail.com>
2017-05-21 21:46:56 -0600

    Update another solution

--- Find_the_Longest_Word_in_a_String.js
+++ Find_the_Longest_Word_in_a_String.js
@@ -3,3 +3,14 @@ function findLongestWord(str) {
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
+
+//Another solution that pull out the longest words
+function findLongestWord(str) {
+     var s = Math.max.apply(null, str.split(" ").map(function(str){return str.length;}).sort());
+     return str.split(" ").filter(function(word){
+              return word.length == s;
+              })   
+}
+
+findLongestWord("The quick brown fox jumped and kicked over the lazy dog");
+// ["jumped", "kicked"]
