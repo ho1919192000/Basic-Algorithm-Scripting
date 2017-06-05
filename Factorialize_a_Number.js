b53e984382efe36ed2a2af8731bb6be5e205d9b7
ho1919192000 <ho1919192000@gmail.com>
2017-06-05 00:25:43 -0600

    Update ternary statement

--- Factorialize_a_Number.js
+++ Factorialize_a_Number.js
@@ -9,3 +9,9 @@ function factorialize(num) {
 
         }
  factorialize(5);
+
+//ternary statement
+function factorialize(num) {
+    return num < 0 ? -1 : num === 0 ? 1 : num * factorialize(num - 1);
+}
+factorialize(5);
